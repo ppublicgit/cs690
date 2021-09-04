520df33a8e4d85809a83bfe1719661a493851266
p.public <p.public@protonmail.com>
2021-09-04 05:36:26 -0500

    Added 4th node for testing

--- main.c
+++ main.c
@@ -11,14 +11,20 @@ int main() {
   printFreeList();
   char *ptr3 = (char *) mymalloc(32);
   printFreeList();
+  char *ptr4 = (char *) mymalloc(64);
+  printFreeList();
+
 
   printf("Freeing...\n");
-  myfree(ptr);
-  printFreeList();
   myfree(ptr2);
   printFreeList();
   myfree(ptr3);
   printFreeList();
+  myfree(ptr);
+  printFreeList();
+  myfree(ptr4);
+  printFreeList();
   printf("Exiting...");
+
   return 0;
 }
